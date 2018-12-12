84e604ae3fc11d58cab43f717b99c2f013095aa1
pyl-woker <panyunlai@126.com>
2018-12-12 14:57:01 +0800

    修复demo_site中的print语句，以支持python3

--- demo_site/upload/models.py
+++ demo_site/upload/models.py
@@ -13,11 +13,11 @@ class Photo(models.Model):
 @receiver(pre_delete, sender=Photo)
 def photo_delete(sender, instance, **kwargs):
     # Pass false so FileField doesn't save the model.  print sender.image
-    print instance
+    print(instance)
     instance.image.delete(False)
 
 def user_directory_path(instance, filename):
-    print "in user_directory_path: " + filename
+    print("in user_directory_path: " + filename)
     return filename
 
 class File(models.Model):
@@ -26,14 +26,14 @@ class File(models.Model):
 
 
     def save(self, *args, **kwargs):
-        print "in save():"
-        print "  filename: " + self.myfile.name
+        print("in save():")
+        print("  filename: " + self.myfile.name)
         count = File.objects.filter(myfile='%s' % self.myfile.name).count()
-        print "  number  : %d" % count
+        print ("  number  : %d" % count)
         #count = File.objects.get(myfile='%s' % self.myfile.name).count()
         if count > 0:
             File.objects.filter(myfile='%s' % self.myfile.name).delete()
-            print "Remove already existing objects from DB"
+            print("Remove already existing objects from DB")
 
         super(File, self).save(*args, **kwargs)
 
@@ -41,8 +41,8 @@ class File(models.Model):
 @receiver(pre_delete, sender=File)
 def file_delete(sender, instance, **kwargs):
     # Pass false so FileField doesn't save the model.
-    print sender.myfile
-    print instance
+    print(sender.myfile)
+    print(instance)
     instance.myfile.delete(False)
 
 @admin.register(Photo)
